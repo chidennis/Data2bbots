9071c75542a5e49054e3d8338c8d30698ee0ff7b
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2021-12-16 00:27:55 +0100

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -13,15 +13,6 @@ provider "aws" {
 	secret_key = "EfmPnexO6dVTsyKhIX9OuWXwrPfOKMCWLAeh5hib"
 }
 
-
-resource "aws_vpc" "myvpc" {
-        cidr_block = "10.0.0.0/16"
-        tags = {
-    Name = "vpc224"
-    Envrionment = "stage"
-  }
-}
-
 resource "aws_instance" "ec2" {
     ami = "ami-0b0af3577fe5e3532"
     instance_type = "t2.micro"
