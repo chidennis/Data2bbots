7fbf3e95f2643b13c3093fde75014b9190289e01
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2021-12-16 17:01:01 +0100

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -10,11 +10,11 @@ terraform {
 provider "aws" {
         region = "us-east-1"
         access_key = "KK"
-	secret_key = "KK"
+	secret_key = "KK/kk"
 }
 
 resource "aws_instance" "ec2" {
-    ami = "ami-0b0af3577fe5e3532"
+    ami = "ami-0bhg860485402"
     instance_type = "t2.micro"
     tags = {
     Name = "Data2bots"
